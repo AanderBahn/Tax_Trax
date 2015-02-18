f0fc35d34ddd11f633b7ae7539b757d9d7033956
Daniel Lopes <danielvlopes@gmail.com>
2015-02-18 15:02:01 -0600

    adjusting devises' layouts to fit apps' layout

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20150212205836) do
     t.datetime "updated_at"
   end
 
+  add_index "trips", ["job_id"], name: "index_trips_on_job_id"
+
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -55,8 +57,15 @@ ActiveRecord::Schema.define(version: 20150212205836) do
 
   create_table "vehicles", force: true do |t|
     t.integer  "user_id"
+    t.string   "make"
+    t.string   "model"
+    t.integer  "year"
+    t.boolean  "personal_usage"
+    t.float    "starting_odometer"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "vehicles", ["user_id"], name: "index_vehicles_on_user_id"
 
 end
