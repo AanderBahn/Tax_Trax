d5672378034ed6f19d8998300544607198cd2d17
Alex Bahn <AanderBahn@gmail.com>
2015-03-16 09:25:23 -0500

    Styling and profiling method calculations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,6 +3,12 @@ class Vehicle < ActiveRecord::Base
   belongs_to :user
   has_many :year_ending_odometers
 
+  def trips_for_year(year)
+    d1 = DateTime.new(year)
+    d2 = d1.end_of_year
+    trips.where(created_at: d1..d2)
+  end
+
   def year_starting_odometer(year)
     last_year_ending = self.year_ending_odometers.find_by(:year => (year - 1))
     if last_year_ending.present?
@@ -13,7 +19,12 @@ class Vehicle < ActiveRecord::Base
   end
 
   def year_ending_odometer(year)
-    return self.year_ending_odometers.find_by(:year => year).reading
+    year_ending_entry = self.year_ending_odometers.find_by(:year => year)
+    if year_ending_entry.present?
+      return year_ending_entry.reading
+    else
+      return trips_for_year(year).last.try(:ending_odometer).to_i
+    end
   end
 
   def total_miles_driven(year)
