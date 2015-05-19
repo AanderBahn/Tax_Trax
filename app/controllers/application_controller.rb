c7684b011544fc8d32cf8f483863a85e0dcece4a
Alexander Bahn <aanderrbahn@Alexanders-MacBook-Pro.local>
2015-05-19 16:19:59 -0500

    device patch 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :avatar
+    devise_parameter_sanitizer.for(:sign_up) << :phone
+  end
 
   def after_sign_out_path_for(resource_or_scope)
     root_path
