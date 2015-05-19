86c15e935e379f44e3f259d706ea16a8e47ae94c
Alexander Bahn <aanderrbahn@Alexanders-MacBook-Pro.local>
2015-05-19 11:33:10 -0500

    device patch 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
-    devise_parameter_sanitizer.for(:sign_up) << :avatar
-    devise_parameter_sanitizer.for(:sign_up) << :phone
-  end
-
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
