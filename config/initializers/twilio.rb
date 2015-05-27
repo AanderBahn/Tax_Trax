0718817558a922edd9b5a4caa092544ba33ae894
Marcus Derencius <derencius@gmail.com>
2015-05-26 21:03:31 -0300

    removing public info

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-  config.account_sid = "AC0440041f9277144fd0b227459e6c1cc6"
-  config.auth_token = "9b3d930adef3770c2c7b495e96aafb5f"
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
 end
