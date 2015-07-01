b3597ba64d8c6c1099afb40ae432aebef9c7a47e
Alexander Bahn <AanderBahn@gmail.com>
2015-07-01 16:03:39 -0500

    Landing Page patch

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,8 @@ Rails.application.routes.draw do
 
   # TRIPS
   resources :trips
-  #get  "/jobs/index"         => "jobs#index"
-  #get  "/trips/new"          => "trips#new"
-  #post "/trips"              => "trips#create"
+
   get  "/trips/:id"           => "trips#show"
-  #get  "/trips/:id/edit"     => "trips#edit"
   post "/trips/:id"           => "trips#update"
   get  "/trips/:id/confirm"   => "trips#confirm"
   get  "/my_trips"            => "trips#my_trips"
@@ -40,36 +37,10 @@ Rails.application.routes.draw do
   get   "/vehicles/:id/confirm"  => "vehicles#confirm"
   get   "/my_vehicles"           => "vehicles#my_vehicles"
 
-  # get   "/vehicles/new"          => "vehicles#new"
-  # post  "/vehicles"              => "vehicles#create"
-  # get   "/vehicles/index"        => "vehicles#index"
-  # get   "/vehicles/:id"          => "vehicles#show"
-
-  # get   '/vehicles/:id/edit', :controller => 'vehicles', :action => 'edit',   :as => 'edit_vehicles'
-  # patch '/vehicles/:id',    :controller => 'vehicles', :action => 'update'
-
-  # post  "/vehicles/:id"          => "vehicles#update"
-  # delete "/vehicles/:id"         => "vehicles#destroy"
-
   #Job
   resources :jobs
-  #get    "/jobs/new"          => "jobs#new"
-  #post   "/jobs"              => "jobs#create"
-  #get    "/jobs/index"        => "jobs#index"
-  #get    "/jobs/:id"          => "jobs#show"
-  #get    "/jobs/:id/edit"     => "jobs#edit"
-  #post   "/jobs/:id"          => "jobs#update"
   get    "/jobs/:id/confirm"  => "jobs#confirm"
   get    "/my_jobs"           => "jobs#my_jobs"
-  #delete "/jobs/:id"          => "jobs#destroy"
-
   post 'twilio/voice' => 'twilio#voice'
 
-  # PROTOTYPE
-  get "/go",      :controller => 'static_pages', :action => 'go'
-  get "/stop",    :controller => 'static_pages', :action => 'stop'
-  get "/log",     :controller => 'static_pages', :action => 'log'
-  get "/confirm", :controller => 'static_pages', :action => 'confirm'
-  get 'static_pages/home'
-  get 'static_pages/help'
 end
